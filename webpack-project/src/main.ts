77c3cbcce55673a6b89920ee39ca959c6119b4a0
Felix dingfeli <dingfeli@amazon.com>
2025-02-07 14:23:42 -0800

    Initial commit

---
+++ webpack-project/src/main.ts
@@ -0,0 +1,10 @@
+import * as vp from 'vite-project';
+
+function main(): void {
+    const a = 1;
+    const b = 2;
+    const res = vp.add(a, b);
+    console.log(res);
+}
+
+main();
---
+++ webpack-project/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
+  },
+};
